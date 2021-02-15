dd12ab504444983e58d1666e220afd8442018430
shawn2776 <=>
2021-02-15 13:09:43 -0800

    finished assignment

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,10 +3,37 @@ export const ADD_ONE = "ADD_ONE";
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
 
-export const addOne = () => {
+export const CLEAR_DISPLAY = 'CLEAR_DISPLAY';
+export const ADD_MEMORY = 'ADD_MEMORY';
+export const UPDATE_MEMORY = 'UPDATE_MEMORY';
+export const CLEAR_MEMORY = 'CLEAR_MEMORY';
+
+const addOne = () => {
     return({type:ADD_ONE});
 }
 
-export const applyNumber = (number) => {
+const applyNumber = (number) => {
     return({type:APPLY_NUMBER, payload:number});
 }
+
+const changeOperation = (sign) => {
+    return({type:CHANGE_OPERATION, payload:sign})
+}
+
+const clearDisplay = () => {
+    return({type:CLEAR_DISPLAY})
+}
+
+const addMemory = () => {
+    return({type:ADD_MEMORY})
+}
+
+const updateMemory = () => {
+    return({type:UPDATE_MEMORY})
+}
+
+const clearMemory = () => {
+    return({type:CLEAR_MEMORY})
+}
+
+export default { addOne, applyNumber, changeOperation, clearDisplay, addMemory, updateMemory, clearMemory }
